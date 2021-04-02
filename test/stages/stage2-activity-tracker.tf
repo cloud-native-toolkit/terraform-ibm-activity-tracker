289658414d7c9a128931f993a8dc9c1ca86931ed
Sean Sundberg <seansund@us.ibm.com>
2021-04-01 23:24:25 -0500

    Adds rg module dependency
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-activity-tracker.tf
+++ test/stages/stage2-activity-tracker.tf
@@ -1,7 +1,7 @@
 module "dev_activity_tracker" {
   source = "./module"
 
-  resource_group_name      = var.resource_group_name
+  resource_group_name      = module.resource_group.name
   resource_location        = var.region
   provision                = true
 }
