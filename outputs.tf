f3c9cdad2dfe269193f7e51b4a23bfea489da21a
Sean Sundberg <seansund@us.ibm.com>
2021-05-11 15:46:10 -0500

    Standardizes interface with other service modules
    - Adds label to input variables
    - Adds id, name, guid, crn, location, service, and label to outputs
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,38 @@
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#  depends_on  = [<some resource>]
-#}
+output "id" {
+  value       = data.ibm_resource_instance.instance.id
+  description = "The id of the provisioned instance."
+}
+
+output "guid" {
+  value       = data.ibm_resource_instance.instance.guid
+  description = "The id of the provisioned instance."
+}
+
+output "name" {
+  value       = local.name
+  depends_on  = [ibm_resource_instance.at_instance]
+  description = "The name of the provisioned instance."
+}
+
+output "crn" {
+  description = "The id of the provisioned instance"
+  value       = data.ibm_resource_instance.instance.id
+}
+
+output "location" {
+  description = "The location of the provisioned instance"
+  value       = var.resource_location
+  depends_on  = [data.ibm_resource_instance.instance]
+}
+
+output "service" {
+  description = "The service name of the key provisioned for the instance"
+  value       = local.service
+  depends_on = [data.ibm_resource_instance.instance]
+}
+
+output "label" {
+  description = "The label for the instance"
+  value       = var.label
+  depends_on = [data.ibm_resource_instance.instance]
+}
