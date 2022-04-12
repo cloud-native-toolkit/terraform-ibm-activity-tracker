fceab1d8990f72b0dfd525ffd33b4f50a6bbdac8
Andrew Trice <amtrice@us.ibm.com>
2022-04-12 11:05:14 -0400

    fixed hardcoded plan, changed to support parallel execution
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage2-activity-tracker.tf
+++ test/stages/stage2-activity-tracker.tf
@@ -5,3 +5,12 @@ module "dev_activity_tracker" {
   resource_location        = "eu-gb"
   ibmcloud_api_key         = var.ibmcloud_api_key
 }
+
+# test to handle parallel execution
+module "dev_activity_tracker2" {
+  source = "./module"
+
+  resource_group_name      = module.resource_group.name
+  resource_location        = "eu-gb"
+  ibmcloud_api_key         = var.ibmcloud_api_key
+}
